dded72393fa68eb392495760e0c600fb27d56f69
Winston Ewert <winstonewert@gmail.com>
2015-10-06 15:19:28 -0700

    Initial project configuration

---
+++ submissions/winstonewert/src/root.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class Root extends React.Component {
+	render() {
+		return <h1>Hello World</h1>
+	}
+}
---
+++ submissions/winstonewert/webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devtool: "eval"		
+};
