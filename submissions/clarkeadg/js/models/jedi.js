5ee970d95810db2b4532bda359c72fd8d9f6a2cc
Brian Clarke <clarkeadg@yahoo.com>
2015-10-09 00:55:27 -0700

    more elegant

--- submissions/clarkeadg/js/models/jedi.js
+++ submissions/clarkeadg/js/models/jedi.js
@@ -4,9 +4,18 @@ var Jedi = (function (App) {
 	return App.Model({
 		id: App.Property(),
 		name: App.Property(),
-		homeworld: App.Property(),
-		master: App.Property(),
-		apprentice: App.Property()
+		homeworld: {
+			id: App.Property(),
+			name: App.Property()
+		},
+		master: {
+			id: App.Property(),
+			url: App.Property()
+		},
+		apprentice: {
+			id: App.Property(),
+			url: App.Property()
+		}
 	});
 
 })(App);
