5c144f5d27d36a2781139e37728fe2b8a168b26e
Brian Clarke <clarkeadg@yahoo.com>
2015-10-08 23:42:16 -0700

    get all jedis

--- submissions/clarkeadg/js/stores/jedi.js
+++ submissions/clarkeadg/js/stores/jedi.js
@@ -2,7 +2,7 @@
 (function (App) {
 
 	App.dispatcher.on('Jedi:getDarthSidious',function(){
-		console.log('Jedi:getDarthSidious');
+		//console.log('Jedi:getDarthSidious');
 		
 		var call = App.config.api.host + App.config.api.get.jedis + App.config.jedis.darthSidious.id;
 		var params = {
@@ -11,17 +11,13 @@
 		App.req.get(call,params,function(data){
 			console.log(data);
 			App._views.Jedis.jedis.push(data);
-			if (data.master) {
-				App.actions.jedi.getMaster(data.master);
-			}
-			if (data.apprentice) {
-				App.actions.jedi.getAprentice(data.apprentice);
-			}
+			getMaster(data);
+			getAprentice(data);
 		});
 	});
 
 	App.dispatcher.on('Jedi:getMaster',function(data){
-		console.log('Jedi:getMaster',data);
+		//console.log('Jedi:getMaster',data);
 		
 		var call = data.url;
 		var params = {
@@ -30,11 +26,12 @@
 		App.req.get(call,params,function(data){
 			console.log('gotMaster',data);
 			App._views.Jedis.jedis.unshift(data);
+			getMaster(data);
 		});
 	});
 
 	App.dispatcher.on('Jedi:getAprentice',function(data){
-		console.log('Jedi:getAprentice',data);
+		//console.log('Jedi:getAprentice',data);
 		
 		var call =  data.url;
 		var params = {
@@ -43,7 +40,20 @@
 		App.req.get(call,params,function(data){
 			console.log('gotAprentice',data);
 			App._views.Jedis.jedis.push(data);
+			getAprentice(data);
 		});
 	});
+
+	function getMaster(data) {
+		if (data.master && data.master.url) {
+			App.actions.jedi.getMaster(data.master);
+		}
+	}
+
+	function getAprentice(data) {
+		if (data.apprentice && data.apprentice.url) {
+			App.actions.jedi.getAprentice(data.apprentice);
+		}
+	}
 
 })(App);
