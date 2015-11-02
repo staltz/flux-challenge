551ed9cef4203bd50bd1600e599df99cb6f8e3a8
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2015-11-02 21:54:12 +0200

    Flux-challenge version with Reagent
    This should be a fully working version that adheres to
    the requirements. Really made me want to try re-frame or
    something similar!

---
+++ submissions/kauko/README.md
@@ -0,0 +1,41 @@
+# kauko
+
+A submission to the frontend flux-challenge by staltz
+
+## Overview
+
+Frontend written in clojurescript
+
+## Setup
+
+To get a clojure environment up and running all you should need is Leiningen: http://leiningen.org/
+
+To get an interactive development environment run:
+
+    lein figwheel
+
+and open your browser at [localhost:3449](http://localhost:3449/).
+This will auto compile and send all changes to the browser without the
+need to reload. After the compilation process is complete, you will
+get a Browser Connected REPL. An easy way to try it is:
+
+    (js/alert "Am I connected?")
+
+and you should see an alert in the browser window.
+
+To clean all compiled files:
+
+    lein clean
+
+To create a production build run:
+
+    lein cljsbuild once min
+
+And open your browser in `resources/public/index.html`. You will not
+get live reloading, nor a REPL. 
+
+## License
+
+Copyright © 2014 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
---
+++ submissions/kauko/src/kauko/requests.cljs
@@ -0,0 +1,39 @@
+(ns kauko.requests
+  (:require [cljs-http.client :as http]
+            [cljs-http.core :refer [abort!]]
+            [cljs.core.async :refer [<! >! chan]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+(def pending-requests (atom {}))
+(def out-channel (chan 10))
+
+(defn- GET! [id]
+  (when-not (get @pending-requests id)
+    (print "Searching for sith with id: " id)
+    (let [request (http/get (str "http://localhost:3000/dark-jedis/" id)
+                            {:with-credentials? false})]
+      (swap! pending-requests assoc id request)
+      request)))
+
+(defn abort-request! [id]
+  (when (get @pending-requests id)
+    (print "Aborting request for " id)
+    (abort! (get @pending-requests id))
+    (swap! pending-requests dissoc id)))
+
+(defn abort-requests! []
+  (print "Abort ALL requests!")
+  (map (fn [[_ req]] (abort! req)) @pending-requests)
+  (reset! pending-requests {}))
+
+(defn get-sith! [id]
+  (let [request (GET! id)]
+    (when request
+      (go
+        (>! out-channel (let [result (<! request)]
+                          (swap! pending-requests dissoc (get-in result [:body :id]))
+                          result))))))
+
+(add-watch pending-requests :foo
+           (fn [_ _ old new]
+             (print (keys old) " => " (keys new))))
