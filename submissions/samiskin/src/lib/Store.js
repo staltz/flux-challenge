0328d71a4b9cc214dbf49fa0371914eb5eb6b960
Shiranka Miskin <shiranka.miskin@gmail.com>
2016-02-27 12:58:02 -0500

    Replaced with redux-flux-store, added bundle.js

--- submissions/samiskin/src/lib/Store.js
+++ submissions/samiskin/src/lib/Store.js
@@ -1,117 +1,33 @@
 import _ from 'lodash';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 // import {devTools} from 'redux-devtools';
+import {fluxEnhancer} from 'redux-flux-store';
 
 import AppStore from 'stores/AppStore';
 import SithStore from 'stores/SithStore';
 
-/*
-   This class stores the main Redux Store and provides a wrapper
-   class to it.  All app state is stored here, and each immediate
-   property can be bound to a Store class.
+let logger = createLogger({
+  level: 'info',
+  duration: true,
+  actionTransformer: (action) => _.assign({}, action, {type: action.type.toString()})
+});
 
-   An example of this would be if all user data was stored in a users property.
-
-    let stateStoreMap = {
-      users: UserStore
-    }
-
- */
-
-let stateStoreMap = {
-  app: AppStore,
-  siths: SithStore
-};
-
-class Store {
-  constructor() {
-    this.initializeStoreMap();
-    this.partiallyReducedState = null;
-    let logger = createLogger({
-      level: 'info',
-      duration: true,
-      actionTransformer: (action) => _.assign({}, action, {type: action.type.toString()})
-    });
-
-    let middleware = [thunk];
-    if (true) {// dev
-      middleware.push(logger);
-      middleware.push(require('redux-immutable-state-invariant')());
-    }
-
-    let createStoreWithMiddleware = applyMiddleware(...middleware)(createStore); // Comment to disable logging
-    this.store = createStoreWithMiddleware(this.reducer.bind(this));
-  }
-
-  // Resolve all store dependencies by inserting into this.storeMap in an order
-  // that will ensure that a store dependant on another will have the other store
-  // resolve all actions first
-  initializeStoreMap() {
-    let invertedMap = _.transform(stateStoreMap, (map, store, prop) => {
-      return map.set(store, prop);
-    }, new Map());
-
-    let waitingSet = new Set();
-    this.storeMap = new Map();
-    _.forEach(stateStoreMap, (store) => waitingSet.add(store));
-
-    let assignStore = (store) => {
-      if (store === undefined || this.storeMap.has(store)) return;
-      if (!waitingSet.has(store)) {
-        throw new Error('Undefined store, check for circular dependencies in storeDependencies');
-      }
-
-      waitingSet.delete(store);
-      if (Array.isArray(store.storeDependencies)) {
-        store.storeDependencies.forEach((dependency) => assignStore(dependency));
-      }
-      this.storeMap.set(store, invertedMap.get(store));
-    };
-
-    while (waitingSet.size > 0) {
-      let store = waitingSet.entries().next().value[0];
-      assignStore(store);
-    }
-  }
-
-  // Redux reducer that passes actions through the store files
-  reduceFromStores(state, action) {
-    let newState = {};
-    this.partiallyReducedState = _.assign({}, state);
-
-    for (let entry of this.storeMap) {
-      let key = entry[1];
-      let store = entry[0];
-
-      if (!store.reduce) throw new Error(`${store.constructor.name} must provide a reduce function`);
-      let value = store.reduce.bind(store)(state[key], action);
-      if (value === undefined) throw new Error(`${store.constructor.name}.reduce must provide a default value`);
-      newState[key] = value; // Default to old value
-      this.partiallyReducedState[key] = newState[key];
-    }
-    this.partiallyReducedState = null;
-
-    return newState;
-  }
-
-  // Returns the current state, evne when in the middle of a reduction
-  getState() {
-    return this.partiallyReducedState || this.store.getState();
-  }
-
-  subscribe(listener) {
-    return this.store.subscribe(listener);
-  }
-
-  dispatch(action) {
-    this.store.dispatch(action);
-  }
-
-  reducer(state = {}, action) {
-    return this.reduceFromStores(state, action);
-  }
+let middleware = [thunk];
+if (true) {// dev
+  middleware.push(logger);
+  middleware.push(require('redux-immutable-state-invariant')());
 }
 
-export default new Store();
+let finalCreateStore = compose(
+  fluxEnhancer({
+    app: AppStore,
+    siths: SithStore
+  }),
+  applyMiddleware(...middleware)
+)(createStore);
+
+let store = finalCreateStore();
+
+export default store;
