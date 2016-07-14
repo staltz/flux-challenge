9e34cb7ec75927c4f7aa6ca3f1972e3d368a8987
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-07-14 09:25:30 +0530

    Added scroll to intent

--- submissions/artfuldev/src/intent.ts
+++ submissions/artfuldev/src/intent.ts
@@ -3,6 +3,9 @@ import { Stream } from 'xstream';
 
 export interface IIntent {
   planet$: Stream<string>;
+  scrollUp$: Stream<boolean>;
+  scrollDown$: Stream<boolean>;
+
 }
 
 function intent(sources: ISources): IIntent {
@@ -10,7 +13,17 @@ function intent(sources: ISources): IIntent {
   const ws = sources.ws;
   const intent = {
     planet$: ws.message$
-      .map(message => (JSON.parse(message) as IPlanet).name)
+      .map(message => (JSON.parse(message) as IPlanet).name),
+    scrollUp$: dom
+      .select('.css-button-up')
+      .events('click')
+      .filter(ev => (ev.target as HTMLElement).className.indexOf('disabled') === -1)
+      .mapTo(true),
+    scrollDown$: dom
+      .select('.css-button-down')
+      .events('click')
+      .filter(ev => (ev.target as HTMLElement).className.indexOf('disabled') === -1)
+      .mapTo(true)
   };
   return intent;
 }
