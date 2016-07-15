59ef7e0ff8c6eed8196b0393bb40e9adcee92a8f
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-07-15 14:54:38 +0530

    Refactored disabling the scroll buttons

--- submissions/artfuldev/src/view.ts
+++ submissions/artfuldev/src/view.ts
@@ -15,22 +15,24 @@ function renderJediSlot(jedi: IJedi): VNode {
   );
 }
 
+function disableIfNotAllowed(allowed: boolean): string {
+ return (allowed ? '' : '.css-button-disabled')
+}
+
 function view(state$: Stream<IApplicationState>): Stream<VNode> {
   const vNode$ =
     state$
       .map(state => {
-        var planetName = '';
-        if (state.planet)
-          planetName = state.planet.name || '';
         const down = state.down;
         const up = state.up;
+        const planetName = (state.planet && state.planet.name) || '';
         return div('.css-root', [
           h1('.css-planet-monitor', 'Obi-Wan currently on ' + planetName),
           section('.css-scrollable-list', [
             ul('.css-slots', state.jedis.map(renderJediSlot)),
             div('.css-scroll-buttons', [
-              button('.css-button-up' + (up ? '' : '.css-button-disabled')),
-              button('.css-button-down' + (down ? '' : '.css-button-disabled'))
+              button('.css-button-up' + disableIfNotAllowed(up)),
+              button('.css-button-down' + disableIfNotAllowed(down))
             ])
           ])
         ]);
